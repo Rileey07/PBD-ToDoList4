c80a86fe2f6a08835eebb5f96b0f9d25e1628bfe
etinurkholifah <etinurkholifah67@gmail.com>
2023-09-17 17:18:17 +0700

    Membuat tabel dan Koneksi database

---
+++ Config/Database.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Config {
+
+    class Database
+    {
+
+        static function getConnection(): \PDO
+        {
+            $host = "localhost";
+            $port = 3306;
+            $database = "pbd_todolist4";
+            $username = "root";
+            $password ="";
+
+
+            return new \PDO("mysql:host=$host:$port;dbnames=$database", $username, $password);
+        }
+
+    }
+
+}
