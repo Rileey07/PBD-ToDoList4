a1d7fb39f1408804bc11001c4f8a9ea19589aac7
RizkyPratama07 <rizkytama1283@gmail.com>
2023-09-22 13:55:40 +0700

    Create ToDoListRepository.php

---
+++ Repository/ToDoListRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Repository{
+
+    use Entity\Todolist;
+
+    interface ToDoLiRepository{
+        
+        function save(Todolist $todolist): void;
+
+        function remove(int $number): bool;
+
+        function findAll(): array;
+    }
+
+    class ToDoListRepository implements ToDoListRepository{
+
+        public array $todolist = array();
+
+        private \PDO $connection;
+
+        public function __construct(\PDO $connection)
+        {
+            $this->$connection = $connection;
+        }
+
+        function save(Todolist $todolist): void{
+            // $number = sizeof($this->todolist) + 1;
+            // $this->todolist($number) = $todolist
+
+            $sql = "INSERT INTO todolist(todo) VALUES (?)";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute([[$todolist->getTodo()]]);
+        }
+
+        function remove(int $number): bool{
+            if ($number > sizeof($this->todolist)){
+                return false;
+            }
+
+            for ($i = $number; $i < sizeof($this->todolist); $i++){
+                $this->todolist[$i] = $this->todolist[$i + 1];
+            }
+
+            unset($this->todolist[sizeof($this->todolist)]);
+
+            return true;
+        }
+
+        function findAll(): array{
+            return $this->todolist;
+        }
+    }
+}
