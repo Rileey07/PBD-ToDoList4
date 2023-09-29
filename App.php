e90deae1754be0653ed6050dfb528e0008de0cde
etinurkholifah <etinurkholifah67@gmail.com>
2023-09-29 20:01:23 +0700

    rr

--- App.php
+++ App.php
@@ -5,10 +5,9 @@ use Repository\TodolistRepository;
 
 require_once __DIR__ . '/Entity/Todolist.php';
 require_once __DIR__ . '/Helper/InputHelper.php';
-require_once __DIR__ . '/Repository/ToDoListRepository.php';
-require_once __DIR__ . '/Service/Todolistview.php';
+require_once __DIR__ . '/Repository/TodolistRepository.php';
+require_once __DIR__ . '/Service/TodolistService.php';
 require_once __DIR__ . '/View/TodolistView.php';
-require_once __DIR__ . '/Entity/Todolist.php';
 require_once __DIR__ . '/Config/Database.php';
 
 use \Repository\TodolistRepositoryImpl;
@@ -19,9 +18,7 @@ echo "Aplikasi Todolist" . PHP_EOL;
 
 $connection =\Config\Database::getConnection();
 $todolistRepository = new TodolistRepositoryImpl($connection);
-$todolistService = new TodolistServiceImpl($TodolistRepository);
+$todolistService = new TodolistServiceImpl($todolistRepository);
 $todolistView = new TodolistView($todolistService);
 
 $todolistView->showTodolist();
-
-?>
